8c06e4668f79f1e7a5144581895b25736ad014ed
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-09-10 13:48:55 -0400

    fixed everyhting

--- api/project/model.js
+++ api/project/model.js
@@ -6,11 +6,19 @@ const addProject = async project => {
         .insert(project)
         .then(([id]) => db("projects")
             .where({ project_id: id })
-        )
+            .first()
+        ).then(project => ({
+            ...project,
+            project_completed: project.project_completed ? true : false
+        }))
 } 
 
 const getProjects = () => {
-    return db("projects")
+    return db("projects").then(projects => 
+        projects.map(p => ({
+            ...p,
+            project_completed: p.project_completed ? true : false
+        })))
 } 
 
 module.exports = {
--- api/task/model.js
+++ api/task/model.js
@@ -8,11 +8,21 @@ const addTask = task => {
             db("tasks")
                 .where({ task_id })
                 .first()
-        )
+        ).then(t => ({
+            ...t,
+            task_completed: t.task_completed ? true : false
+        }))
 }
 
 const getTasks = () => {
     return db("tasks")
+    .leftJoin("projects as p", "p.project_id", "tasks.project_id")
+    .then(tasks => 
+        tasks.map(t => ({
+            ...t,
+            task_completed: t.task_completed ? true : false
+        }))
+    )
 }
 
 module.exports = {  
